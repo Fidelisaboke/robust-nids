7a0eb4fde08a2f369dfe7a517d59e5107f2ebd4b
Fidelisaboke <fidelisaboke@gmail.com>
2025-10-05 08:06:28 +0300

    feat: add NIDS placeholder routers - Added endpoint for model inference - Added endpoint for adversarial training - Added NIDS alerts endpoints

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 
-from .routers import auth, users
+from .routers import auth, nids, users
 
 app = FastAPI(title='Robust NIDS API', version='1.0.0')
 
 # Include API routers
 app.include_router(auth.router)
 app.include_router(users.router)
+app.include_router(nids.router)
+
 
 
 @app.get('/')
